8c9ad6b30c4fc6d9af2b0cffbbd3336deeb3fdc3
Furkan <Furkan@FURKAN-PC>
2023-05-31 18:27:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+string metin = "abcçdefgğhıijklmnoöprsştuüvyz";
+
+for (int i = 0;i < metin.Length; i++)
+{
+    Console.WriteLine(metin[i] + "-" + metin[i].ToString().ToUpper()+" ");
+}
